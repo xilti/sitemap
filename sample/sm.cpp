404a86e989a21d3a13bab60da82a37be193d2b9a
egladysh <egladysh@Yelenas-MacBook-Pro.local>
2017-12-16 16:14:26 -0800

    change

--- sample/sm.cpp
+++ sample/sm.cpp
@@ -1,12 +1,36 @@
-#include <iterator>
 #include <sitemap/sitemap.h>
+
+
+sitemap g_site;
 
 int main()
 {
-	sitemap sm;
+	// Site files
+	g_site["home"] = { //file label
+		"/index.html" //destination file
+	};
 
-	sm["test"] = {"/test.txt", []() { std::string s("test\n\n"); std::vector<unsigned char> v; std::copy(s.begin(), s.end(), std::back_inserter(v)); return v;}};
-	sm.set_root("./");
-	sm.generate("");
+	// Content function, it must return std::vector<unsigned int>
+	auto f = []() 
+	{ 
+		std::string s("<html>"); 
+		s += "<body>";
+		s += "<p>Hello World!</p>";
+		s += "<a href='" + g_site["home"].dst() + "'>Home</a>";
+		s += "</body>";
+
+		std::vector<unsigned char> v; 
+		std::copy(s.begin(), s.end(), std::back_inserter(v)); return v;
+	};
+	
+	// Content definition
+	g_site["home"] = f;
+
+	// Site root directory
+	//
+	g_site.set_root("./sample_site/");
+
+	// Generate it
+	g_site.generate("");
 	return 0;
 }
